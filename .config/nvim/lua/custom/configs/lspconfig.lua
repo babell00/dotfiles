c08a910ef1638c475843fa38bf27e1ab7185e903
Jakub Gajzler <jgajzler@bol.com>
2024-03-10 13:05:24 +0100

    add nvim custom config

---
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,44 @@
+local config = require("plugins.configs.lspconfig")
+local on_attach = config.on_attach
+local capabilities = config.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.eslint.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.tailwindcss.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.cssls.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = { "rust" },
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+  setting = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  }
+})
+
+lspconfig.taplo.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+})
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "eslint-lsp",
+        "prettierd",
+        "typescript-language-server",
+        "tailwindcss-language-server"
+      }
+    }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "rust",
+        "json",
+        "markdown",
+        "python",
+        "sql",
+        "toml",
+        "xml",
+        "css"
+      }
+      return opts
+    end,
+  },
+  {
+    "michaelrommel/nvim-silicon",
+    lilazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        font = "JetBrainsMono Nerd Font=24",
+        theme = "Dracula"
+      })
+    end
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    even = "VeryLazy",
+    opts = function()
+      return require("custom.configs.null-ls")
+    end
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+}
